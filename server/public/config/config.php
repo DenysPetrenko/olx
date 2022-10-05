616c81f6256f735e873fd8626ff72ea7cc0f1f57
petrenko.denys <des.petrenko@gmai.com>
2022-10-05 11:59:41 +0300

    first commit

---
+++ server/public/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dns' => "mysql:host=olx_mysql;port=3306;dbname=olx",
+    'username' => 'myapp',
+    'password' => 'myapp',
+];
+
---
+++ server/public/connDb.php
@@ -0,0 +1,13 @@
+<?php
+
+try {
+
+
+    $db = require 'config/config.php';
+    $conn = new PDO($db['dns'], $db['username'], $db['password']);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
